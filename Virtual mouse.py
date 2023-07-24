b4f6a076324b5fba3cb8cce22f6ba8643d044b80
Ullas P <112563367+Ullasachar16@users.noreply.github.com>
2023-07-24 19:50:00 +0530

    Commit

--- Virtual mouse.py
+++ Virtual mouse.py
@@ -19,7 +19,6 @@ mouseLoc=np.array([0,0])
 DampingFactor=2 # should be greater than 1
 pinchFlag=0
 openx,openy,openw,openh=(0,0,0,0)
-
 while True:
     ret,img=cap.read()
     #img=cv2.resize(img,(360,240))
